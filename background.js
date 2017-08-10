24896d928a577a0ce217ff23c6077ef8b19309bf
Vu Le <thenewvu@gmail.com>
2017-08-10 16:58:36 +0700

    Remove persisting last window position

--- background.js
+++ background.js
@@ -1,24 +1,8 @@
-chrome.app.runtime.onLaunched.addListener(() => {
-  chrome.storage.local.get(outerBounds => {
-    chrome.app.window.create(
-      'index.html',
-      {
-        outerBounds,
-        alwaysOnTop: true,
-        frame: {
-          type: 'none'
-        }
-      },
-      createdWindow => {
-        createdWindow.onBoundsChanged.addListener(() => {
-          chrome.storage.local.set({
-            width: createdWindow.outerBounds.width,
-            height: createdWindow.outerBounds.height,
-            top: createdWindow.outerBounds.top,
-            left: createdWindow.outerBounds.left
-          })
-        })
-      }
-    )
-  })
-})
+/* global chrome */
+
+function onAppLoaded() {
+  const winOpts = { id: Date.now().toString(), alwaysOnTop: true, frame: { type: 'none' } }
+  chrome.app.window.create('index.html', winOpts)
+}
+
+chrome.app.runtime.onLaunched.addListener(onAppLoaded)
